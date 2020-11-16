f58aab450baf5849b39280e6a0354e582dc9eb5f
Ben Winkelman <benwinkelman@gmail.com>
2020-11-15 19:56:45 -0500

    Initial Commit

---
+++ ear training app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ear training app
+//
+//  Created by Ben Winkelman on 11/15/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
