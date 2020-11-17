a971f46dd07a9579527f320b49f4265da97817ae
Massimo Biolcati <massimobio@icloud.com>
2020-11-17 16:37:46 -0500

    Rename main table view controller

--- ear training app/Classes/MainTableViewController.swift
+++ ear training app/Classes/MainTableViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 import AVFoundation
 
-class ViewController: UITableViewController {
+class MainTableViewController: UITableViewController {
     var intervals = [String]()
     
     var mp3 = String()
