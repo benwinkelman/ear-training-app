5180f92e4d178a0ee69475cc8823be272e39c492
Ben Winkelman <benwinkelman@gmail.com>
2020-11-19 13:20:51 -0500

    changed so it can select three types of fifths

--- ear training app/Classes/DetailViewController.swift
+++ ear training app/Classes/DetailViewController.swift
@@ -16,13 +16,14 @@ class DetailViewController: UIViewController {
     private var intervalNumber = 0
     private var correctAnswer = 0
     private var numberOfIntervals = 0
+    private var numberOfIntervalTypes = 0
     
     @IBOutlet var firstAnswerButton: UIButton!
     @IBOutlet var secondAnswerButton: UIButton!
     @IBOutlet var thirdAnswerButton: UIButton!
     @IBOutlet var answerButtonsStackView: UIStackView!
     
-    // odd numbered intervals are minor (or perfect for 4th and 5th), even are major (or augmented for 4th and 5th) - only problem is would like to include diminshed fifth as well.
+    // odd numbered intervals are minor (or perfect for 4th and 5th), even are major (or augmented for 4th and 5th), except for 5ths (1 for dim, 2, for perfect, 3 for aug, add 3 as you go up)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,6 +34,7 @@ class DetailViewController: UIViewController {
         thirdAnswerButton.isHidden = true
         answerButtonsStackView.axis = .horizontal
         numberOfIntervals = 2
+        numberOfIntervalTypes = 2
 
         switch selectedInterval {
         case "second":
@@ -47,6 +49,8 @@ class DetailViewController: UIViewController {
             thirdAnswerButton.setTitle("augmented", for: .normal)
             thirdAnswerButton.isHidden = false
             answerButtonsStackView.axis = .vertical
+            numberOfIntervalTypes = 3
+            numberOfIntervals = 6
             
         default:
             break
@@ -71,7 +75,7 @@ class DetailViewController: UIViewController {
     
     @IBAction func buttonTapped(_ sender: UIButton) {
         var title: String
-        if sender.tag == (intervalNumber - 1) % 2 {
+        if sender.tag == (intervalNumber - 1) % numberOfIntervalTypes {
             title = "Correct"
         } else {
             title = "Wrong"
